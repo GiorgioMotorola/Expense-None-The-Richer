e4c6d4fb9055e46fd62639d13da0fec0bd9077de
Mike Weatherford <97712526+GiorgioMotorola@users.noreply.github.com>
2024-03-09 22:23:16 -0500

    added data, added binding forrrr data

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,11 +1,27 @@
 <template>
      <h3>History</h3>
-      <ul id="list" class="list">
-        <li class="minus">
-          Cash <span>-$400</span><button class="delete-btn">x</button>
+     <ul id="list" class="list">
+        <li v-for="transaction in transactions" 
+        :key="transaction.id" 
+        :class="transaction.amount < 0 ? 'minus' : 'plus'"
+        >
+            {{ transaction.text }}<span>${{ transaction.amount }}</span>
+            <button class="delete-btn">x</button>
         </li>
-        <li class="plus">
-          Paycheck <span>$800</span><button class="delete-btn">x</button>
-        </li>
-      </ul>
+     </ul>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                transactions: [
+                    { id: 1, text: 'Flower', amount: -19.99 },
+                    { id: 2, text: 'Salary', amount: 299.99 },
+                    { id: 3, text: 'Book', amount: -10 },
+                    { id: 4, text: 'Camera', amount: 150 },
+                ]
+            }
+        }
+    }
+</script>
