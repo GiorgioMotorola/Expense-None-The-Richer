76a7b59201691dc3e8ab251bb4e8e5fd84f63adc
Mike Weatherford <97712526+GiorgioMotorola@users.noreply.github.com>
2024-03-11 19:53:19 -0400

    added computed functions for income and expenses

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 <Header />
 <div class="container">
   <Balance :total="total" />
-  <IncomeExpenses />
+  <IncomeExpenses :income="income" :expenses="expenses"/>
   <TransactionList :transactions="transactions" />
   <AddTransaction />
 </div>
@@ -24,9 +24,31 @@ const transactions = ref([
         { id: 4, text: 'Camera', amount: 150 },
     ]);
 
+    // get total
     const total = computed(() => {
       return transactions.value.reduce((acc, transaction) => {
         return acc + transaction.amount;
       }, 0);
-    })
+    });
+
+    // get income
+    const income = computed(() => {
+      return transactions.value
+      .filter((transaction) => transaction.amount > 0)
+      .reduce((acc, transaction) => {
+        return acc + transaction.amount;
+      }, 0)
+      .toFixed(2);
+    });
+
+
+    // get expenses
+    const expenses = computed(() => {
+      return transactions.value
+      .filter((transaction) => transaction.amount < 0)
+      .reduce((acc, transaction) => {
+        return acc + transaction.amount;
+      }, 0)
+      .toFixed(2);
+    });
 </script>
