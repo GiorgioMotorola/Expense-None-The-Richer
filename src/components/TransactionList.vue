eb3a65ac249ff54ef4142b6fd2342f8c64ca23a5
Mike Weatherford <97712526+GiorgioMotorola@users.noreply.github.com>
2024-03-11 19:33:49 -0400

    added ref, computed, defineprops

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -11,17 +11,13 @@
      </ul>
 </template>
 
-<script>
-    export default {
-        data() {
-            return {
-                transactions: [
-                    { id: 1, text: 'Flower', amount: -19.99 },
-                    { id: 2, text: 'Salary', amount: 299.99 },
-                    { id: 3, text: 'Book', amount: -10 },
-                    { id: 4, text: 'Camera', amount: 150 },
-                ]
-            }
-        }
-    }
+<script setup>
+   import { defineProps } from 'vue';
+
+    const props = defineProps ({
+        transactions: {
+            type: Array, 
+            required: true,
+        },
+    });
 </script>
