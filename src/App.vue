c10559bfc6ed0096e42d6b6afe096fcbe07f0657
Mike Weatherford <97712526+GiorgioMotorola@users.noreply.github.com>
2024-03-12 20:41:22 -0400

    added local storage

--- src/App.vue
+++ src/App.vue
@@ -16,16 +16,19 @@ import TransactionList from './components/TransactionList.vue';
 import AddTransaction from './components/AddTransaction.vue';
 import { useToast } from 'vue-toastification';
 
-import { ref, computed } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 
 const toast = useToast();
 
-const transactions = ref([
-        { id: 1, text: 'Flower', amount: -19.99 },
-        { id: 2, text: 'Salary', amount: 299.97 },
-        { id: 3, text: 'Book', amount: -10 },
-        { id: 4, text: 'Camera', amount: 150 },
-    ]);
+const transactions = ref([]);
+
+onMounted(() => {
+  const savedTransactions = JSON.parse(localStorage.getItem('transactions'));
+
+  if(savedTransactions) {
+    transactions.value = savedTransactions;
+  }
+});
 
     // get total
     const total = computed(() => {
@@ -62,6 +65,7 @@ const transactions = ref([
         text: transactionData.text,
         amount: transactionData.amount,
       });
+      saveTransactionsToLocalStorage();
       toast.success('Transaction Added!');
     };
 
@@ -75,6 +79,12 @@ const transactions = ref([
       transactions.value = transactions.value.filter((transaction) => 
         transaction.id !== id
       );
+      saveTransactionsToLocalStorage();
       toast.success('Transaction Deleted!');
     };
+
+    //Save to Local Storage
+    const saveTransactionsToLocalStorage = () => {
+      localStorage.setItem('transactions', JSON.stringify(transactions.value));
+    }
 </script>
