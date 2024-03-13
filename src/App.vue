f0edb3c26b65949ac1579449e6b3b912960b6903
Mike Weatherford <97712526+GiorgioMotorola@users.noreply.github.com>
2024-03-12 20:07:19 -0400

    aded toastification

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 <Header />
 <div class="container">
   <Balance :total="total" />
-  <IncomeExpenses :income="income" :expenses="expenses"/>
+  <IncomeExpenses :income="+income" :expenses="+expenses"/>
   <TransactionList :transactions="transactions" />
   <AddTransaction />
 </div>
--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -1,17 +1,35 @@
 <template>
      <h3>Add new transaction</h3>
-      <form id="form">
+      <form id="form" @submit.prevent="onSubmit">
         <div class="form-control">
           <label for="text">Text</label>
-          <input type="text" id="text" placeholder="Enter text..." />
+          <input type="text" id="text" v-model="text" placeholder="Enter text..." />
         </div>
         <div class="form-control">
           <label for="amount"
             >Amount <br />
             (negative - expense, positive - income)</label
           >
-          <input type="number" id="amount" placeholder="Enter amount..." />
+          <input type="text" id="amount" v-model="amount" placeholder="Enter amount..." />
         </div>
         <button class="btn">Add transaction</button>
       </form>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+import { useToast } from 'vue-toastification';
+
+const toast = useToast();
+
+const text = ref('');
+const amount = ref('');
+const onSubmit = () => {
+  if(!text.value || !amount.value) {
+    toast.error('Both Fields Must be Filled!');
+    return;
+  }
+  text.value = '';
+  amount.value = '';
+}
+</script>
